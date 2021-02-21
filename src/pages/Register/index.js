33c103b17fcae62059e3ec3bf60e37bd148946c7
Cici Saepudin <cici.syaepudin@gmail.com>
2021-02-21 18:37:33 +0700

    add flash message

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,71 +1,91 @@
 import React, {useState} from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {Header, Input, Button, Gap, Loading} from '../../components';
-import { Fire } from '../../config';
+import {Fire} from '../../config';
 import {colors, useForm} from '../../utils';
+import {showMessage, hideMessage} from 'react-native-flash-message';
 
 const Register = ({navigation}) => {
-  const [form,setForm] = useForm({
-    fullName:'',
-    profession:'',
-    email:'',
-    password:''
+  const [form, setForm] = useForm({
+    fullName: '',
+    profession: '',
+    email: '',
+    password: '',
   });
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const onContinue = () => {
     setLoading(true);
-    Fire.auth().createUserWithEmailAndPassword(form.email, form.password)
-    .then((userCredential) => {
-      // Signed in 
-      var user = userCredential.user;
-      setLoading(false);
-    })
-    .catch((error) => {
-      // const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log('register',errorMessage)
-      setLoading(false);
-    });
+    Fire.auth()
+      .createUserWithEmailAndPassword(form.email, form.password)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        const dataUser = {
+          fullName: form.fullName,
+          email: form.email,
+          profession: form.profession,
+        };
+
+        Fire.database()
+          .ref('users/' + user.uid + '/')
+          .set(dataUser);
+
+        setForm('reset-value');
+        setLoading(false);
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('register', errorMessage);
+        setForm('reset-value');
+        setLoading(false);
+        showMessage({
+          message: errorMessage,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
   };
 
   return (
     <>
-    <View style={styles.page}>
-      <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
-      <View style={styles.content}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Input
-            label="Full Name"
-            value={form.fullName}
-            onChangeText={(value) => setForm('fullName',value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Pekerjaan"
-            value={form.profession}
-            onChangeText={(value) => setForm('profession',value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Email"
-            value={form.email}
-            onChangeText={(value) => setForm('email',value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Password"
-            value={form.password}
-            secureTextEntry
-            onChangeText={(value) => setForm('password',value)}
-          />
-          <Gap height={40} />
-          <Button onPress={onContinue} title="Continue" />
-        </ScrollView>
+      <View style={styles.page}>
+        <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
+        <View style={styles.content}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Input
+              label="Full Name"
+              value={form.fullName}
+              onChangeText={(value) => setForm('fullName', value)}
+            />
+            <Gap height={24} />
+            <Input
+              label="Pekerjaan"
+              value={form.profession}
+              onChangeText={(value) => setForm('profession', value)}
+            />
+            <Gap height={24} />
+            <Input
+              label="Email"
+              value={form.email}
+              onChangeText={(value) => setForm('email', value)}
+            />
+            <Gap height={24} />
+            <Input
+              label="Password"
+              value={form.password}
+              secureTextEntry
+              onChangeText={(value) => setForm('password', value)}
+            />
+            <Gap height={40} />
+            <Button onPress={onContinue} title="Continue" />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-    {loading && <Loading/>}
+      {loading && <Loading />}
     </>
   );
 };
--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -7,8 +7,9 @@ const mainColors = {
   grey1: '#7d8797',
   grey2: '#E9e9e9',
   blue1: '#0066CB',
-  black1:'#000000',
-  black2:'rgba(0,0,0,0.5)'
+  black1: '#000000',
+  black2: 'rgba(0,0,0,0.5)',
+  red1: '#E06379',
 };
 
 export const colors = {
@@ -37,5 +38,6 @@ export const colors = {
   },
   border: mainColors.grey2,
   cardLight: mainColors.green2,
-  loadingBackground : mainColors.black2
+  loadingBackground : mainColors.black2,
+  error:mainColors.red1
 };
